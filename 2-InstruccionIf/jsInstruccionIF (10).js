a16f2560eed63d4dcbaac6d1ef97180b4ddc4901
MatiasVelazco23 <matiasvelazco23@gmail.com>
2020-02-14 16:12:17 -0300

    Tarea

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,22 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numero;
+	maximo=10;
+	minimo=1;
 	
+	numero= Math.floor(Math.random()* ((maximo+1)-minimo)+minimo);
+
+if(numero==10 || numero==9)
+	{
+		alert("Excelente")
+	}
+	else if (numero<4){
+		alert("vamos, la próxima se puede")
+	}
+	else
+	{
+	alert("Aprobó");
+}
 
 }//FIN DE LA FUNCIÓN
