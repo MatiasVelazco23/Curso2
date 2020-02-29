6752859f86af3701462a7fe842b54792d95cce54
MatiasVelazco23 <60776295+MatiasVelazco23@users.noreply.github.com>
2020-02-29 13:41:52 -0300

    Update jsEntradaSalida-3.js

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -3,8 +3,8 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
     var nombre;
-    nombre=document.getElementById("elNombre").value;
-    alert(nombre);
+    nombre=document.getElementById("elNombre").value; //tomar una id del html
+    alert(nombre);                                     //mostrar dato sacado de la id
 }
 
 
